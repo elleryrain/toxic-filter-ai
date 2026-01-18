fa9fad7203421887cfa89a5cc64c426f9be6415d
elleryrain <3ll3ry.r41n@gmail.com>
2026-01-19 01:34:48 +0300

    project: initialized repo feat: add tsconfig file feat: create dataset.ru.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,6 @@
+export type TLabel = 0 | 1;
+
+export type TSample = {
+  text: string;
+  label: TLabel;
+};
---
+++ src/train.ts
@@ -0,0 +1,2 @@
+import * as tf from "@tensorflow/tfjs-node";
+import * as use from "@tensorflow-models/universal-sentence-encoder";
